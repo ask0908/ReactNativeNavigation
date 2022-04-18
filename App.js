bee0373f22db50156be8edb2184da2c93b1a2bee
ask0908 <askdfjaslk@naver.com>
2022-04-18 23:45:13 +0900

    273p까지 코드 작성함. 공부 내용 노션에 정리

--- App.js
+++ App.js
@@ -1,39 +1,22 @@
 import * as React from 'react';
-import {Text} from 'react-native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import MainScreen from './screens/MainScreen';
+import DetailScreen from './screens/DetailScreen';
 
-const Tab = createBottomTabNavigator();
-
-function HomeScreen() {
-    return <Text>Home</Text>;
-}
-
-function SearchScreen() {
-    return <Text>Search</Text>;
-}
-
-function NotificationScreen() {
-    return <Text>Notification</Text>;
-}
-
-function MessageScreen() {
-    return <Text>Message</Text>;
-}
+const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Tab.Navigator initialRouteName="Home">
-                <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Search" component={SearchScreen} />
-                <Tab.Screen
-                    name="Notification"
-                    component={NotificationScreen}
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Main"
+                    component={MainScreen}
+                    options={{headerShown: false}}
                 />
-                <Tab.Screen name="Message" component={MessageScreen} />
-            </Tab.Navigator>
+                <Stack.Screen name="Detail" component={DetailScreen} />
+            </Stack.Navigator>
         </NavigationContainer>
     );
 }
