3b38961fd80ad2f272af9d6f6c85e56b96b910ea
ask0908 <askdfjaslk@naver.com>
2022-04-18 09:42:58 +0900

    init

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+function App() {
+  return <NavigationContainer>{/* 네비게이션 설정 */}</NavigationContainer>;
+}
+
+export default App;
